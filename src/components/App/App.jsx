71c22c9ee6416988d6d90fe626289f5ef1195575
fairycreator <122693095+fairycreator@users.noreply.github.com>
2023-07-31 23:10:51 +0200

    complete

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,40 +1,79 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://hn.algolia.com/api/v1';
-
-const ArticleList = ({ articles }) => (
-  <ul>
-    {articles.map(({ objectID, url, title }) => (
-      <li key={objectID}>
-        <a href={url} target="_blank" rel="noreferrer noopener">
-          {title}
-        </a>
-      </li>
-    ))}
-  </ul>
-);
+import Searchbar from '../Searchbar/Searchbar';
+import ImageGallery from '../ImageGallery/ImageGallery';
+import Button from '../Button/Button';
+import Loader from '../Loader/Loader';
+import Modal from '../Modal/Modal';
+import api from '../Api/Api';
+import '../Styles/styles.css';
 
 class App extends Component {
   state = {
-    articles: [],
+    images: [],
     isLoading: false,
+    isError: false,
+    error: null,
+    showModal: false,
+    modalImageURL: '',
+    searchQuery: 'react',
+    currentPage: 1,
   };
 
-  async componentDidMount() {
-    this.setState({ isLoading: true });
-    const response = await axios.get('/search?query=react');
-    this.setState({
-      articles: response.data.hits,
-      isLoading: false,
-    });
+  componentDidMount() {
+    this.fetchImages();
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchQuery !== this.state.searchQuery) {
+      this.setState({ images: [], currentPage: 1 }, this.fetchImages);
+    }
+  }
+
+  fetchImages = async () => {
+    const { searchQuery, currentPage } = this.state;
+    this.setState({ isLoading: true });
+
+    try {
+      const images = await api.fetchImages(searchQuery, currentPage);
+      this.setState(prevState => ({
+        images: [...prevState.images, ...images],
+        currentPage: prevState.currentPage + 1,
+        isLoading: false,
+      }));
+    } catch (error) {
+      this.setState({ isError: true, error, isLoading: false });
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  handleFormSubmit = query => {
+    this.setState({ searchQuery: query });
+  };
+
+  handleImageClick = largeImageURL => {
+    this.setState({ showModal: true, modalImageURL: largeImageURL });
+  };
+
+  closeModal = () => {
+    this.setState({ showModal: false, modalImageURL: '' });
+  };
+
   render() {
-    const { articles, isLoading } = this.state;
+    const { images, isLoading, isError, showModal, modalImageURL } = this.state;
+
     return (
       <div>
-        isLoading ? <p>Loading...</p> : <ArticleList articles={articles} />
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        {isError && <div>Error: {this.state.error.message}</div>}
+        <ImageGallery images={images} onClick={this.handleImageClick} />
+        {isLoading && <Loader />}
+        {showModal && (
+          <Modal largeImageURL={modalImageURL} onClose={this.closeModal} />
+        )}
+        {images.length > 0 && !isLoading && !isError && (
+          <Button onLoadMore={this.fetchImages} show={true} />
+        )}
       </div>
     );
   }
